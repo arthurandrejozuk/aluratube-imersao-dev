e02e6f3c14e7975248dd26024de936eaf109397c
Arthur Andrejozuk Ávila <107406724+arthurandrejozuk@users.noreply.github.com>
2025-05-20 12:40:59 -0300

    Retirando Public key e Projeto url

--- app/services/videoService.js
+++ app/services/videoService.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const PROJETO_URL = "https://scpsabciznwgbkctdfyp.supabase.co";
-const PUBLIC_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNjcHNhYmNpem53Z2JrY3RkZnlwIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTA4ODQ2NDIsImV4cCI6MjAwNjQ2MDY0Mn0.lY6l2EYF-rrH9PnDHwwhpHu9SFRZ1ec4ZiSJ2YOPXUU"
+const PROJETO_URL = process.env.PROJETO_URL;
+const PUBLIC_KEY = process.env.PUBLIC_KEY;
 const supabase = createClient(PROJETO_URL, PUBLIC_KEY);
 
 
